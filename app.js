a52dffd97667c5db6e30481944c9976dfa0e11dc
Nikita <azazzzzz9.9@gmail.com>
2024-07-04 21:03:13 +0300

    1

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ btn1.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
+		tg.MainButton.setText("Вы выбрали товар 4343!");
 		item = "1";
 		tg.MainButton.show();
 	}
